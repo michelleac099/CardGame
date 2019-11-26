e20c1f2995d525a593f6cd9a5b161b8941256ced
Michelle Chis <s1230292@monmouth.edu>
2019-11-25 20:03:37 -0500

    Some code written.

--- CardGame/src/CardGame.java
+++ CardGame/src/CardGame.java
@@ -1,9 +1,42 @@
-
+import java.util.Random;
 public class CardGame {
 
 	public static void main(String[] args) {
-		// initial commit
-
+		int[] myHand = new int[5];
+		int[] yourHand = new int[5];
+		String mySuit = " ";
+		String yourSuit = " ";
+		Random generator = new Random();
+		int card = 0;
+		int countH = 0;
+		int countD = 0;
+		int countC = 0;
+		int countS = 0;
+		boolean winner = false;
+		
+		for(int i = 0; i<5; i++) {
+			card = 1 + generator.nextInt(52);
+			myHand[i]=card;
+			System.out.println("My hand: " + myHand[i]);
+			
+			card = 1+generator.nextInt(52);
+			yourHand[i]=card;
+			System.out.println("Your hand: " + yourHand[i]);
+		} //for
+		
+		for (int j = 0; j<5; j++) {
+			if (myHand[j]>=1 && myHand[j]<=13)
+				countH++;
+			else if (myHand[j]>=14 && myHand[j]<=26)
+				countD++;
+			else if (myHand[j]>=27 && myHand[j]<=39)
+				countC++;
+			else if (myHand[j]>=40 && myHand[j]<=52)
+				countS++;
+		} //for
+		
+		
+		
 	} //main
 
 } //class
