d95b570a3befd52d46287164356b3ed8fb3df862
Michelle Chis <s1230292@monmouth.edu>
2019-11-25 20:28:19 -0500

    Working on beginning the game loop.

--- CardGame/src/CardGame.java
+++ CardGame/src/CardGame.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Arrays;
 public class CardGame {
 
 	public static void main(String[] args) {
@@ -6,6 +7,7 @@ public class CardGame {
 		int[] yourHand = new int[5];
 		String mySuit = " ";
 		String yourSuit = " ";
+		String drawSuit = " ";
 		Random generator = new Random();
 		int card = 0;
 		int countH = 0;
@@ -14,6 +16,7 @@ public class CardGame {
 		int countS = 0;
 		boolean winner = false;
 		
+		//Generating hands
 		for(int i = 0; i<5; i++) {
 			card = 1 + generator.nextInt(52);
 			myHand[i]=card;
@@ -32,10 +35,76 @@ public class CardGame {
 			else if (myHand[j]>=27 && myHand[j]<=39)
 				countC++;
 			else if (myHand[j]>=40 && myHand[j]<=52)
-				countS++;
+				countS++;			
 		} //for
 		
+		//Setting mySuit
+		if(countH>countD && countH>countC && countH>countS)
+			mySuit = "Hearts";
+		else if (countD>countH && countD>countC && countD>countS)
+			mySuit = "Diamonds";
+		else if (countC>countD && countC>countH && countC>countS)
+			mySuit = "Clubs";
+		else if (countS>countD && countS>countC && countS>countH)
+			mySuit = "Spades";
 		
+		countH=0;
+		countD=0;
+		countC=0;
+		countS=0;
+		
+		for (int j = 0; j<5; j++) {
+			if (yourHand[j]>=1 && yourHand[j]<=13)
+				countH++;
+			else if (yourHand[j]>=14 && yourHand[j]<=26)
+				countD++;
+			else if (yourHand[j]>=27 && yourHand[j]<=39)
+				countC++;
+			else if (yourHand[j]>=40 && yourHand[j]<=52)
+				countS++;
+		}//for
+		
+		//Setting yourSuit
+		if(countH>countD && countH>countC && countH>countS)
+			yourSuit = "Hearts";
+		else if (countD>countH && countD>countC && countD>countS)
+			yourSuit = "Diamonds";
+		else if (countC>countD && countC>countH && countC>countS)
+			yourSuit = "Clubs";
+		else if (countS>countD && countS>countC && countS>countH)
+			yourSuit = "Spades";
+
+		System.out.println("My hand has five cards: " + Arrays.toString(myHand));
+		System.out.println("Your hand has five cards: " + Arrays.toString(yourHand));
+		System.out.println("My suit: " + mySuit);
+		System.out.println("Your suit: " + yourSuit);
+		
+		/*
+		 * Start of game play loop code.
+		 * Win condition: A player has all 5 cards in their hand matching their suit.
+		 */
+		
+		while(winner=false) {
+			
+			card = 1 + generator.nextInt(52);
+			if (card>=1 && card<=13)
+				drawSuit = "Hearts";
+			else if (card>=14 && card<=26)
+				drawSuit = "Diamonds";
+			else if (card>=27 && card<=39)
+				drawSuit = "Clubs";
+			else if (card>=40 && card<=52)
+				drawSuit = "Spades";
+			
+			if (drawSuit.contentEquals(mySuit))
+				/*
+				 * check my existing cards. If I don’t have that card # add it to my hand.
+				*Add it to my hand by replacing a card that is not in my suit.
+				*/
+				System.out.println("edit this");
+			
+			
+		} //while
 		
 	} //main
 
